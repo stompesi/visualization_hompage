816dd71d6fb156fa3ad0d139223f532eafc58c0c
HAN JONGBEEN <stompesi@gmail.com>
2018-12-21 20:34:39 +0900

    AA

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,11 @@ router.get('/', (req, res) => {
     res.render('index.ejs')
 });
 
+router.get('/project', (req, res) => {
+    res.render('project.ejs')
+});
+
+router.get('/team', (req, res) => {
+    res.render('team.ejs')
+});
 exports.router = router;
